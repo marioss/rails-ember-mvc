5c317b02ab69f32c2a499f80d4f6b1169e3990cc
Mario Stefano Suharja <me@mario.sg>
2013-07-07 22:20:50 +0700

    Ember MVC Rails 4  - Initialize --Mario

---
+++ app/assets/javascripts/router.js.coffee
@@ -0,0 +1,5 @@
+# For more information see: http://emberjs.com/guides/routing/
+
+EmberTodomvc.Router.map ()->
+  # @resource('posts')
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmberTodomvc::Application.config.secret_key_base = 'db5954076b225e7adb5eab74577ef52b3a995ec6b9ca5a7dfd92bc8203c3b5af54e91977eec69d5e270e1582d95f8d882d46eade80ece2723ae4ef6293da6b4e'
